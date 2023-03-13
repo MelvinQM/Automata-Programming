1d6d638eed9ef57ad59ab3360d2b28df78d960ee
Melvin Moes <melvin.moes@hva.nl>
2023-03-13 20:56:33 +0100

    Contiuned work on week 5 assignment

--- src/week5/Files.java
+++ src/week5/Files.java
@@ -1,5 +1,5 @@
 package week5;
-import week2.huisdier.Pet;
+import week2.huisdier.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -30,7 +30,7 @@ public class Files {
         PrintWriter pw = new PrintWriter(path);
 
         for(Pet pet : pets){
-            pw.printf("%s", pets.getClass());
+            pw.printf("%s\n", pet.getName());
         }
         pw.close();
     }
@@ -42,6 +42,53 @@ public class Files {
         }
 
     }
+    public void addPet(String animalName, String name, ArrayList<Pet> pets) throws InterruptedException {
+        switch (animalName){
+            case "Dog":
+                pets.add(new Dog(name));
+                pets.get(pets.size() - 1);
+                break;
+            case "Cat":
+                pets.add(new Cat(name));
+                break;
+            case "Chihuahua":
+                pets.add(new Chihuahua(name));
+                break;
+            case "Poodle":
+                pets.add(new Poodle(name));
+                break;
+            case "Tiger":
+                pets.add(new Tiger(name));
+                break;
+            default:
+                System.out.println("Error");
 
+        }
+        System.out.printf("%s the %s created! Make sure to save your changes!\n\n", name, animalName);
+        Thread.sleep(1000);
+    }
+    public void deletePet(String animalName, String name, ArrayList<Pet> pets) throws InterruptedException {
+        switch (animalName){
+            case "Dog":
+                pets.remove(name);
+                break;
+            case "Cat":
+                pets.remove(new Cat(name));
+                break;
+            case "Chihuahua":
+                pets.remove(new Chihuahua(name));
+                break;
+            case "Poodle":
+                pets.remove(new Poodle(name));
+                break;
+            case "Tiger":
+                pets.remove(new Tiger(name));
+                break;
+            default:
+                System.out.println("Error");
 
+        }
+        System.out.printf("%s the %s deleted! Make sure to save your changes!\n\n", name, animalName);
+        Thread.sleep(1000);
+    }
 }
--- src/week5/Main.java
+++ src/week5/Main.java
@@ -9,18 +9,58 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
         Files file = new Files("bestand.txt");
-
+        Scanner s = new Scanner(System.in);
         //Making an Arraylist of pet objects
         ArrayList<Pet> pets = new ArrayList<>();
             pets.add(new Dog("Karel"));
             pets.add(new Cat("Cerel"));
 
-        //Write out all data into the file
-        file.insertData(pets);
-        //Read all data in the file
-        file.readData();
-
+        String name;
+        String animalName;
+        while(true){
+            System.out.println("Choose between these options:");
+            System.out.println("Choose 1 to add a Pet to the file");
+            System.out.println("Choose 2 to edit a Pet from the file");
+            System.out.println("Choose 3 to delete a pet from the file");
+            System.out.println("Choose R to read the file");
+            System.out.println("Choose S to save the file");
+            char option = s.next().charAt(0);
+            switch (option){
+                //Add
+                case '1':
+                    System.out.println("Choose an animal from this list (Cat, Dog, Poodle, Chihuahua, Tiger)");
+                    System.out.print("My animal is a: ");
+                    animalName = s.next();
+                    System.out.print("What is his or her name:");
+                    name = s.next();
+                    file.addPet(animalName, name, pets);
+                    break;
+                //Edit
+                case '2':
+                    break;
+                //Delete
+                case '3':
+                    System.out.println("Choose an animal from this list to delete(Cat, Dog, Poodle, Chihuahua, Tiger)");
+                    System.out.print("My animal is a: ");
+                    animalName = s.next();
+                    System.out.print("What is his or her name:");
+                    name = s.next();
+                    file.deletePet(animalName, name, pets);
+                    break;
+                //Save
+                case 'S'|'s':
+                    file.insertData(pets);
+                    break;
+                //Read
+                case 'R'|'r':
+                    file.readData();
+                    break;
+                default:
+                    System.out.println("Wrong input try again");
+            }
+        }
     }
 }
+
